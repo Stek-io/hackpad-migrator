2d2a6abf6fc94f89b563faae191902b60824d931
Nikolaos Prodromidis <nikolaos.prodromidis@gmail.com>
2017-07-04 12:28:23 +0300

    Quote image names (to eliminate spaces) and add try catch blocks

--- image_uploader.py
+++ image_uploader.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.client import Config
 import mimetypes
 from datetime import datetime, timedelta
-import urllib.request
+import urllib.parse, urllib.request
 from PIL import Image
 import io
 from bs4 import BeautifulSoup
@@ -11,8 +11,8 @@ bucket_name = 'stekpad'
 
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-def replace_image(html, bucket_name, http_scheme):
-    # parse html and add it in a var - will help to find images efficiently and replace the src attribute
+def replace_image(html, bucket_name, http_scheme='https'):
+    # parse html and put it in a variable - BeautifulSoup parses html efficiently
     soup = BeautifulSoup(html, "html.parser")
 
     # run loop for all images in the html
@@ -20,33 +20,45 @@ def replace_image(html, bucket_name, http_scheme):
     for image in soup.findAll('img'):
         image_src = image['src']
 
-        # read image url
-        file = io.BytesIO(urllib.request.urlopen(image_src).read())
-        img = Image.open(file, mode='r')
-
-        # stream file in binary mode
-        imgByteArr = io.BytesIO()
-        img.save(imgByteArr, format='PNG')
-        imgByteArr = imgByteArr.getvalue()
-
-        # get image name
-        image_url_parts = image_src.split('/')
-        image_name = image_url_parts
-
-        # get image mime_type
+        #get image mime_type
         mime_type_info = mimetypes.guess_type(image_src)
-        mime_type = mime_type_info[0] if mime_type_info[0] else 'binary/octet-stream'
+        mime_type = mime_type_info[0] if mime_type_info[0] else 'image/jpeg'
 
-        # construct expire and cache control headers
+        # construct expire and cache_control headers
         days=100
         cache_control = 'max-age= %d' % (60 * 60 * 24 * days)
         expires = datetime.utcnow() + timedelta(days=days)
         expires = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
-        # upload images to our bucket
-        s3.Bucket(bucket_name).put_object(Key=image_name[-1], Body=imgByteArr, ACL='public-read', ContentType=mime_type, CacheControl=cache_control,Expires=expires)
+        try:
+            # get image name
+            image_url_parts = image_src.split('/')
+            image_name = image_url_parts
 
-        # replace the src of the image with the new uploaded location
-        image['src'] = http_scheme+'://s3.eu-central-1.amazonaws.com/'+bucket_name+'/'+image_name[-1]
+            # read image url
+            image_src_parsed = urllib.parse.urlparse(image_src)
+            image_name_encoded = urllib.parse.quote(image_src_parsed.path)
 
-        return soup
+            file = io.BytesIO(urllib.request.urlopen(urllib.parse.urljoin(image_src, image_name_encoded)).read())
+            img = Image.open(file, mode='r')
+        except urllib.error.HTTPError as error:
+            try:
+                print(image_src)
+                file = io.BytesIO(urllib.request.urlopen(image_src).read())
+                img = Image.open(file, mode='r')
+            except urllib.error.HTTPError as error:
+                print(error.read())
+                break
+
+            # stream file in binary mode
+            imgByteArr = io.BytesIO()
+            img.save(imgByteArr, format='PNG')
+            imgByteArr = imgByteArr.getvalue()
+
+            # upload image to our bucket
+            s3.Bucket(bucket_name).put_object(Key=image_name[-1], Body=imgByteArr, ACL='public-read', ContentType=mime_type, CacheControl=cache_control,Expires=expires)
+
+            # replace the src of the image with the new uploaded location
+            image['src'] = http_scheme+'://s3.eu-central-1.amazonaws.com/'+bucket_name+'/'+image_name[-1]
+
+    return soup
